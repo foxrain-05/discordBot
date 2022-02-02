bdd2ec51a16c51a8290dbafcbb88606d0d120f02
foxrain <88512219+foxrain-05@users.noreply.github.com>
2022-02-03 06:39:48 +0900

    /ping 명령어 적용

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('서버의 ping을 확인합니다!'),
-    async execute(interaction){
-        await interaction.reply("ms");
+    async execute(interaction, client){
+        await interaction.reply(`${client.ws.ping}ms`);
     },
 };
